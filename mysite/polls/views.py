bfe521fc27923482a60da04da9881bada5f7077f
kashif <kashifiqbal0214@gmail.com>
2023-08-10 09:03:29 +0500

    Added a Automated testing in test.py I have some changes in views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,23 +2,27 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
 
 # Create your views here.
 class IndexView(generic.ListView):
-    template_namem = "polls/index.html"
+    template_name = "polls/index.html"
     context_object_name = "latest_question_lists"
     
     def get_queryset(self):
 
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
     
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte= timezone.now())
 
 
 class ResultsView(generic.DetailView):
